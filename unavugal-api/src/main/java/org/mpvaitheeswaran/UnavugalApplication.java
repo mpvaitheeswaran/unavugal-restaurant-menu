7123452927f5668afedcca8ba683c9fd5b337e93
Vaitheeswaran <vaitheeswaran.p@bluescopetech.com>
2024-01-01 20:03:25 +0530

    Initial api commit.

---
+++ unavugal-api/src/main/java/org/mpvaitheeswaran/UnavugalApplication.java
@@ -0,0 +1,11 @@
+package org.mpvaitheeswaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnavugalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnavugalApplication.class,args);
+    }
+}
